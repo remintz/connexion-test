cd0c4fa3d0de1bbf8c5ac13b7666dddf526cfdfc
Renato Mintz <remintz@gmail.com>
2017-05-15 12:08:14 -0300

    Not working yet

---
+++ api/lockersets/__init__.py
@@ -0,0 +1,8 @@
+lockersets = {
+    0: {"name": "First item"}
+}
+
+
+def search() -> list:
+    print('888888888 SEARCH')
+    return lockersets
--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import logging.config
-
+import connexion
+from connexion.resolver import RestyResolver
 from flask import Flask, Blueprint
-from smartlocker_api import settings
+import settings
 from smartlocker_api.api.endpoints.lockersets import ns as lockersets_namespace
 #from smartlocker_api.api.endpoints.users import ns as users_namespace
 from smartlocker_api.api.restplus import api
 from smartlocker_api.database import db, reset_database
 
 
-app = Flask(__name__)
+app = connexion.FlaskApp(__name__, 9090, specification_dir='swagger/')
 logging.config.fileConfig('logging.conf')
 log = logging.getLogger(__name__)
-
 
 def configure_app(flask_app):
     flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
@@ -27,18 +27,20 @@ def configure_app(flask_app):
 def initialize_app(flask_app):
     configure_app(flask_app)
 
-    blueprint = Blueprint('api', __name__, url_prefix='/api')
-    api.init_app(blueprint)
-    api.add_namespace(lockersets_namespace)
-    flask_app.register_blueprint(blueprint)
+#    blueprint = Blueprint('api', __name__, url_prefix='/api')
+#    api.init_app(blueprint)
+#    api.add_namespace(lockersets_namespace)
+#    flask_app.register_blueprint(blueprint)
     db.init_app(flask_app)
     if settings.RESET_DATABASE:
         reset_database(flask_app)
 
 
 def main():
-    initialize_app(app)
-    log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
+    flask_app = app.app
+    initialize_app(flask_app)
+    log.info('>>>>> Starting development server at http://{}/ <<<<<'.format(flask_app.config['SERVER_NAME']))
+    app.add_api('smartlocker_api.yaml', resolver=RestyResolver('api'))
     app.run(debug=settings.FLASK_DEBUG)
 
 if __name__ == "__main__":
